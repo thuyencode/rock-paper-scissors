f4b26067732fc287de790a74bb57d9660085a63c
thuyencode <thuyencode@proton.me>
2023-11-13 10:35:21 +0700

    Add base URL

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
+  base: '/rock-paper-scissors/',
   plugins: [
     createHtmlPlugin({
       minify: true,
